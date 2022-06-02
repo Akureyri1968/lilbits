331661ce03518b3087cf86723c645d26b80e2437
Akureyri1968 <gummisel@gmail.com>
2022-06-02 09:37:38 +0000

    Updates

---
+++ precache-manifest.b70b59ac6586003de1e58e21f817b624.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd497bfa9a7406d7f93d",
+    "url": "/lilbits.git/static/css/main.3d29e9a3.chunk.css"
+  },
+  {
+    "revision": "dd497bfa9a7406d7f93d",
+    "url": "/lilbits.git/static/js/main.dd497bfa.chunk.js"
+  },
+  {
+    "revision": "2bc649a667ccbd873989",
+    "url": "/lilbits.git/static/js/1.2bc649a6.chunk.js"
+  },
+  {
+    "revision": "6821cdf283e9672cace5",
+    "url": "/lilbits.git/static/css/2.4cbc15e2.chunk.css"
+  },
+  {
+    "revision": "6821cdf283e9672cace5",
+    "url": "/lilbits.git/static/js/2.6821cdf2.chunk.js"
+  },
+  {
+    "revision": "7dc35b9232e2049043e7",
+    "url": "/lilbits.git/static/js/runtime~main.7dc35b92.js"
+  },
+  {
+    "revision": "ffeae9bfe23c1acf7947a015408b38f8",
+    "url": "/lilbits.git/static/media/playfair-display-cyrillic-400-normal.ffeae9bf.woff2"
+  },
+  {
+    "revision": "27711e3ac37f6bfbf1db51d7e250df19",
+    "url": "/lilbits.git/static/media/playfair-display-vietnamese-400-normal.27711e3a.woff2"
+  },
+  {
+    "revision": "ef740e9cacffe9147b3e150686c33259",
+    "url": "/lilbits.git/static/media/playfair-display-latin-ext-400-normal.ef740e9c.woff2"
+  },
+  {
+    "revision": "2afc074b0a28a247a63a4bf7821476ee",
+    "url": "/lilbits.git/static/media/playfair-display-latin-400-normal.2afc074b.woff2"
+  },
+  {
+    "revision": "ef0f537f65590f2c5146c3f34c96c36b",
+    "url": "/lilbits.git/static/media/playfair-display-all-400-normal.ef0f537f.woff"
+  },
+  {
+    "revision": "a4429774144b058df86f816e54cfce36",
+    "url": "/lilbits.git/static/media/background.a4429774.jpg"
+  },
+  {
+    "revision": "a2bd52f253e6f42d641921bed30a261e",
+    "url": "/lilbits.git/static/media/carousel2.a2bd52f2.jpg"
+  },
+  {
+    "revision": "f6f8f5e4c6f8c764cb72cf091209c622",
+    "url": "/lilbits.git/static/media/carousel3.f6f8f5e4.jpg"
+  },
+  {
+    "revision": "4bcf9db794e276dfc4ef3506944ae63a",
+    "url": "/lilbits.git/static/media/lilbits.4bcf9db7.png"
+  },
+  {
+    "revision": "39db31086e30e55093f2499a55e7b34c",
+    "url": "/lilbits.git/index.html"
+  }
+];
